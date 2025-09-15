63a7616fae6c916656bbbd3d8230c62d8b5d3e2f
Lukas <lukasmanell@gmail.com>
2025-09-15 17:11:04 +0000

    Repetitions 8th Try

--- Repetitions.java
+++ Repetitions.java
@@ -1,4 +1,5 @@
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Function;
@@ -10,42 +11,20 @@ public class Repetitions {
 
         String sequence = scanner.nextLine();
 
-        int maxRunLength = Integer.MIN_VALUE;
+        Map<Character, Integer> maxRunLengths = new HashMap<>();
+        maxRunLengths.put('A', 0);
+        maxRunLengths.put('C', 0);
+        maxRunLengths.put('G', 0);
+        maxRunLengths.put('T', 0);
 
-        Map<Character, Integer> frequencyMap = sequence.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        c -> 1,
-                        Integer::sum)
-                );
-        if (frequencyMap.size() <= 4) {
-            System.out.println(Collections.max(frequencyMap.entrySet(), Map.Entry.comparingByValue()).getValue());
-            return;
-        }
-
-        if (sequence.length() - sequence.replace(sequence.substring(0, 1), "").length() == sequence.length() - 1) {
-            System.out.println(sequence.length());
-            return;
-        }
-
-        for (int i = 0; i < sequence.length(); i++) {
-            int runLength = 0;
-            boolean reachedEnd = false;
-            for (int j = i; j < sequence.length(); j++) {
-                if (sequence.charAt(i) != sequence.charAt(j)) {
-                    break;
-                }
-                if (j == sequence.length() - 1) {
-                    reachedEnd = true;
-                }
-                runLength++;
-            }
-            maxRunLength = Math.max(maxRunLength, runLength);
-            if (reachedEnd) {
-                break;
+        int start = 0;
+        for (int i = 0; i < sequence.length() - 1; i++) {
+            char startChar = sequence.charAt(start);
+            if (sequence.charAt(i + 1) != startChar) {
+                maxRunLengths.put(startChar, Math.max(maxRunLengths.get(startChar), i - start));
+                start = i + 1;
             }
         }
-        System.out.println(maxRunLength);
+        System.out.println(Collections.max(maxRunLengths.entrySet(), Map.Entry.comparingByValue()).getValue());
     }
 }
