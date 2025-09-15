e11c75f21b8c5fca8cd526cc39aa28ab23291145
Lukas <lukasmanell@gmail.com>
2025-09-15 16:06:02 +0000

    MissingNumbers 6th Try

--- MissingNumber.java
+++ MissingNumber.java
@@ -8,11 +8,11 @@ public class MissingNumber {
         long n = scanner.nextInt();
         scanner.nextLine();
         String numList = scanner.nextLine();
-        // n*(n-1) / 2
+        // n*(n+1) / 2
         long sumNums = Arrays.stream(numList.split("\\s+"))
                 .mapToLong(Long::parseLong)
                 .sum();
-        long output = n * (n - 1) / 2 - sumNums;
+        long output = n * (n + 1) / 2 - sumNums;
         System.out.println(output);
     }
 }
