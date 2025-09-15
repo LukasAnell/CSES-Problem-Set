62cba6f59042d0229ce44c8fa916016f47101ae6
Lukas <lukasmanell@gmail.com>
2025-09-15 14:53:51 +0000

    WeirdAlgorithm 6th Try

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -4,15 +4,14 @@ public class WeirdAlgorithm {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int startingNum = scanner.nextInt();
-        int currentNum = startingNum;
-        while (currentNum >= 2) {
-            System.out.printf("%d ", currentNum);
+        long startingNum = scanner.nextLong();
+        while (startingNum >= 2) {
+            System.out.printf("%d ", startingNum);
 
-            if (currentNum % 2 == 0) {
-                currentNum /= 2;
+            if (startingNum % 2 == 0) {
+                startingNum /= 2;
             } else {
-                currentNum = currentNum * 3 + 1;
+                startingNum = startingNum * 3 + 1;
             }
         }
 
