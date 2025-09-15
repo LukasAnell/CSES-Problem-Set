d0f5971b2223f174091b55af1eee634f3421f94c
Lukas <lukasmanell@gmail.com>
2025-09-15 14:40:11 +0000

    WeirdAlgorithm

---
+++ WeirdAlgorithm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startingNum = scanner.nextInt();
+        int currentNum = startingNum;
+        while (currentNum >= 4) {
+            System.out.printf("%d ", currentNum);
+
+            if (currentNum % 2 == 0) {
+                currentNum /= 2;
+            } else {
+                currentNum = currentNum * 3 + 1;
+            }
+        }
+        System.out.println("4 2 1");
+    }
+}
