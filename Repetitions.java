d3014a8686bb79bd7a9b42bca3e87111ec47a78d
Lukas <lukasmanell@gmail.com>
2025-09-15 22:45:49 +0000

    Update imports

--- Repetitions.java
+++ Repetitions.java
@@ -2,8 +2,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Repetitions {
     public static void main(String[] args) {
