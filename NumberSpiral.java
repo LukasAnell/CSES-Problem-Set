67c6ef1e316b9711614359fc80c7ff26332dc16a
Lukas <lukasmanell@gmail.com>
2025-09-18 14:59:49 +0000

    NumberSpiral 4th Try

--- NumberSpiral.java
+++ NumberSpiral.java
@@ -8,30 +8,30 @@ public class NumberSpiral {
         int numTests = scanner.nextInt();
         scanner.nextLine();
 
-        String[] tests = new String[numTests];
+        long[] output = new long[numTests];
         for (int i = 0; i < numTests; i++) {
-            tests[i] = scanner.nextLine();
-        }
+            String[] coordinates = scanner.nextLine().split("\\s+");
 
-        for (String test : tests) {
-            String[] coordinates = test.split("\\s+");
-
-            long y = Integer.parseInt(coordinates[0]);
-            long x = Integer.parseInt(coordinates[1]);
+            long y = Long.parseLong(coordinates[0]);
+            long x = Long.parseLong(coordinates[1]);
 
             if (y >= x) {
                 if (y % 2 == 0) {
-                    System.out.println((long) (Math.pow(y, 2)) - (x - 1));
+                    output[i] = (long) (Math.pow(y, 2)) - (x - 1);
                 } else {
-                    System.out.println((long) (Math.pow(y - 1, 2)) + (y - x) + 1);
+                    output[i] = (long) (Math.pow(y - 1, 2)) + (y - x) + 1;
                 }
             } else {
                 if (x % 2 == 0) {
-                    System.out.println((long) (Math.pow(x - 1, 2)) + (x - y) + 1);
+                    output[i] = (long) (Math.pow(x - 1, 2)) + (x - y) + 1;
                 } else {
-                    System.out.println((long) (Math.pow(x, 2)) - (y - 1));
+                    output[i] = (long) (Math.pow(x, 2)) - (y - 1);
                 }
             }
+        }
+
+        for (long line : output) {
+            System.out.println(line);
         }
     }
 }
