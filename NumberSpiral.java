5ddefa7eeefcebfa86077e7db0e1af3a2a956e53
Lukas <lukasmanell@gmail.com>
2025-09-18 15:17:39 +0000

    NumberSpiral 8th Try

--- NumberSpiral.java
+++ NumberSpiral.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class NumberSpiral {
@@ -16,19 +15,24 @@ public class NumberSpiral {
             long y = Long.parseLong(test.substring(0, spaceIndex));
             long x = Long.parseLong(test.substring(spaceIndex + 1));
 
-            if (y >= x) {
-                if (y % 2 == 0) {
-                    output.append(y * y - (x - 1)).append("\n");
+            long n = Math.max(x, y);
+            long base = (n - 1) * (n - 1);
+
+            if (n % 2 == 0) {
+                if (x == n) {
+                    output.append(n * n - y + 1);
                 } else {
-                    output.append((y - 1) * (y - 1) + (y - x) + 1).append("\n");
+                    output.append(base + x);
                 }
             } else {
-                if (x % 2 == 0) {
-                    output.append((x - 1) * (x - 1) + (x - y) + 1).append("\n");
+                if (y == n) {
+                    output.append(n * n - x + 1);
                 } else {
-                    output.append(x * x - (y - 1)).append("\n");
+                    output.append(base + y);
                 }
             }
+
+            output.append("\n");
         }
 
         System.out.println(output);
