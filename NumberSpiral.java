c9d65027c9da934c98ab57676c7d7ac57e2a7979
Lukas <lukasmanell@gmail.com>
2025-09-18 15:08:14 +0000

    NumberSpiral 6th Try

--- NumberSpiral.java
+++ NumberSpiral.java
@@ -8,7 +8,7 @@ public class NumberSpiral {
         int numTests = scanner.nextInt();
         scanner.nextLine();
 
-        long[] output = new long[numTests];
+        StringBuilder output = new StringBuilder();
         for (int i = 0; i < numTests; i++) {
             String test = scanner.nextLine();
             int spaceIndex = test.indexOf(" ");
@@ -18,21 +18,19 @@ public class NumberSpiral {
 
             if (y >= x) {
                 if (y % 2 == 0) {
-                    output[i] = y * y - (x - 1);
+                    output.append(y * y - (x - 1));
                 } else {
-                    output[i] = (y - 1) * (y - 1) + (y - x) + 1;
+                    output.append((y - 1) * (y - 1) + (y - x) + 1);
                 }
             } else {
                 if (x % 2 == 0) {
-                    output[i] = (x - 1) * (x - 1) + (x - y) + 1;
+                    output.append((x - 1) * (x - 1) + (x - y) + 1);
                 } else {
-                    output[i] = x * x - (y - 1);
+                    output.append(x * x - (y - 1));
                 }
             }
         }
 
-        for (long line : output) {
-            System.out.println(line);
-        }
+        System.out.println(output);
     }
 }
